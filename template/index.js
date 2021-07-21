b31ccf18138ccddcb50d2ddb54a0cd5335088cff
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2021-07-20 21:09:47 -0500

    recoil config

--- template/index.js
+++ template/index.js
@@ -1,9 +1,17 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
+import {RecoilRoot} from 'recoil';
+
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => (
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
+);
+
+AppRegistry.registerComponent(appName, () => Root);
