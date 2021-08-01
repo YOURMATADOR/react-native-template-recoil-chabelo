c87aad9008de48d9a5a9f50a191926914f41b6d5
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2021-07-31 23:29:34 -0500

    update to use yarn v3

--- template.config.js
+++ template.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // Placeholder used to rename and replace in files
   // package.json, index.json, android/, ios/
   placeholderName: "ProjectName",
-
+  titlePlaceholder: 'Project Display Name',
   // Directory with template
   templateDir: "./template",
 
